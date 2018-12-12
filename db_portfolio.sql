730ab243658815365c50f3fb3ff19b51df888d1f
smalfoy <shadiamalfoy@gmail.com>
2018-12-12 15:27:24 -0500

    reimported db

--- db_portfolio.sql
+++ db_portfolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 09, 2018 at 07:02 AM
+-- Generation Time: Dec 12, 2018 at 08:25 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_portfolio`
+-- Database: `db_portfolio_1`
 --
 
 -- --------------------------------------------------------
