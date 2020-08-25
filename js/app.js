2a4be28f6949521e92c03ad6e463eef1de5dbf6d
shadiaali <shadia@shadiaali.ca>
2020-08-24 21:22:04 -0400

    nav and landing

--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-  'use strict';
-  var link = document.querySelector('[data-toggle-menu]');
-  link.addEventListener('click', function () {
-    if (link.classList.contains('toggle-menu--clicked')) {
-      link.classList.remove('toggle-menu--clicked');
-    } else {
-      link.classList.add('toggle-menu--clicked');
-    }
-  }, false);
-}, false);
+
 
 $(function () {
   $(document).scroll(function () {
