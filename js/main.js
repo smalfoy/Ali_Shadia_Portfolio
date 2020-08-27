9995507d2e847ad2b590119fff5132b4a0122fa6
shadiaali <shadia@shadiaali.ca>
2020-08-27 19:30:36 -0400

    modals

--- js/main.js
+++ js/main.js
@@ -3,19 +3,7 @@
     el: '#app',
 
     data: {
-      mainmessage: "Boozhoo! Nice to meet you! My name is Shadia Ursula. I am a full stack web developer who is passionate about bringing ideas to life - from conceptualization to creation. I graduated with a diploma in Interactive Media Design in 2019, and I am currently studying Interactive Media Specialist at Fanshawe's downtown campus.",
-      secondarymessage: "I've been enthusiastic about web development and design from a young age: in my spare time in grade-school you could find me in the library learning HTML4 and CSS2. Although I've progressed a bit beyond that by now, the desire to create still holds strong. I am a critical thinker who is always driven to learn more - because I believe learning is a lifelong process.",
-      aboutheading: "About Me",
-      toolsheader: "Tools",
-      toolssub: "I am adept at various frameworks and am always learning new ones, however I believe it's important to have a solid understanding of the languages the frameworks are built upon.",
-      threefacts: "3 Random Facts",
-      one: "I enjoy Stephen King novels",
-      two: "My clan's totem is the Thunderbird.",
-      three: "Halloween is my favourite celebration.",
-      itpath: "images/it.svg",
-      thunderbirdpath: "images/thunderbird.svg",
-      kittypath: "images/kitty.svg",
-      workheading: "My Work",
+      
 
       portfoliodata: [],
       singleportfoliodata: [],
